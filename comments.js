6594611b99da792a1fbc133d2687b3675b6f8b02
Damandeep Kaur <99991787+damank16@users.noreply.github.com>
2024-07-30 22:20:21 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,31 @@
-// create web server
-//Server created
+//Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var comments = require('./comments.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//GET /comments
+app.get('/comments', function(req, res) {
+    res.json(comments);
+});
+
+//POST /comments
+app.post('/comments', function(req, res) {
+    var comment = req.body;
+    comments.push(comment);
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(comments);
+        }
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Server is running on port 3000');
+});
